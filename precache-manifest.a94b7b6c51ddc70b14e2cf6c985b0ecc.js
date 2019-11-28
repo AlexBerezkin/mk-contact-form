b197bb4eb18bcbc7960fe32999d14cc94410ee65
AlexBerezkin <mail.berezkin@gmail.com>
2019-11-28 04:58:58 -0800

    Updates

---
+++ precache-manifest.a94b7b6c51ddc70b14e2cf6c985b0ecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ed65d0f805e99b5e80ee47a5b2b6b0",
+    "url": "/AlexBerezkin/mk-contact-form.git/index.html"
+  },
+  {
+    "revision": "094fd330ee3cc009dc83",
+    "url": "/AlexBerezkin/mk-contact-form.git/static/css/main.478a14b0.chunk.css"
+  },
+  {
+    "revision": "5e3d7e698a3382615b76",
+    "url": "/AlexBerezkin/mk-contact-form.git/static/js/2.4d6c3474.chunk.js"
+  },
+  {
+    "revision": "094fd330ee3cc009dc83",
+    "url": "/AlexBerezkin/mk-contact-form.git/static/js/main.c735d447.chunk.js"
+  },
+  {
+    "revision": "0a4b744b51b290972975",
+    "url": "/AlexBerezkin/mk-contact-form.git/static/js/runtime-main.41302e27.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AlexBerezkin/mk-contact-form.git/precache-manifest.a94b7b6c51ddc70b14e2cf6c985b0ecc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AlexBerezkin/mk-contact-form.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
