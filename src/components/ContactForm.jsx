22e5720dab70882498ae11788cab6c45657ec6e0
AlexBerezkin <36063651+AlexBerezkin@users.noreply.github.com>
2019-11-28 21:11:38 -0800

    added POST request to API Gateway

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -28,7 +28,31 @@ export class ContactForm extends Component {
         //event.preventDefault();
 
         const { userName, email, message } = this.state;
-        console.log(this.state);
+        
+        try {
+            const data = await postData('https://1u5leuz6gg.execute-api.us-west-2.amazonaws.com/prod/submit', { userName, email, message });
+            console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
+          } catch (error) {
+            console.error(error);
+          }
+          
+          async function postData(url = '', data = {}) {
+            // Default options are marked with *
+            const response = await fetch(url, {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'cors', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'same-origin', // include, *same-origin, omit
+              headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrer: 'no-referrer', // no-referrer, *client
+              body: JSON.stringify(data) // body data type must match "Content-Type" header
+            });
+            return await response.json(); // parses JSON response into native JavaScript objects
+        }
     }
 
     // Handle fields change
@@ -36,7 +60,7 @@ export class ContactForm extends Component {
         const { name, value } = event.target;
 
         this.setState({ [name]: value });
-        console.log(this.state);
+        
     }
 
     render() {
